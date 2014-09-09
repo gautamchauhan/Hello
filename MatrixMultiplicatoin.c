bf35a234ccf4402e3a7bf9e079f986d889d13bff
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 00:55:46 -0300

    Update MatrixMultiplicatoin.c

--- MatrixMultiplicatoin.c
+++ MatrixMultiplicatoin.c
@@ -2,39 +2,32 @@
  
 int main()
 {
-  int m, n, p, q, i, j, k, sum = 0;
+  int size, i, j, k, sum = 0;
   int first[10][10], second[10][10], multiply[10][10];
  
   printf("Enter the number of rows and columns of first matrix\n");
-  scanf("%d%d", &m, &n);
+  scanf("%d", &size);
   printf("Enter the elements of first matrix\n");
  
-  for (  i = 0 ; i < m ; i++ )
-    for ( j = 0 ; j < n ; j++ )
+  for (  i = 0 ; i < size ; i++ )
+    for ( j = 0 ; j < size ; j++ )
     {
       scanf("%d", &first[i][j]);
     }
  
-  printf("Enter the number of rows and columns of second matrix\n");
-  scanf("%d%d", &p, &q);
- 
-  if ( n != p )
-    printf("Matrices with entered orders can't be multiplied with each other.\n");
-  else
-  {
     printf("Enter the elements of second matrix\n");
  
-    for ( i = 0 ; i < p ; i++ )
-      for ( j = 0 ; j < q ; j++ )
+    for ( i = 0 ; i < size ; i++ )
+      for ( j = 0 ; j < size ; j++ )
       {
         scanf("%d", &second[i][j]);
       }
  
-    for ( i = 0 ; i < m ; i++ )
+    for ( i = 0 ; i < size ; i++ )
     {
-      for ( j = 0 ; j < q ; j++ )
+      for ( j = 0 ; j < size ; j++ )
       {
-        for ( k = 0 ; k < p ; k++ )
+        for ( k = 0 ; k < size ; k++ )
         {
           sum = sum + first[i][k]*second[k][j];
         }
@@ -46,14 +39,13 @@ int main()
  
     printf("Product of entered matrices:-\n");
  
-    for ( i = 0 ; i < m ; i++ )
+    for ( i = 0 ; i < size ; i++ )
     {
-      for ( j = 0 ; j < q ; j++ )
+      for ( j = 0 ; j < size ; j++ )
         printf("%d\t", multiply[i][j]);
  
       printf("\n");
     }
-  }
  
   return 0;
 }
