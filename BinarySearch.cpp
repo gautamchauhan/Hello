b50574e0f6c47295927d19d85379b6c34aa1f699
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 00:33:31 -0300

    Update BinarySearch.cpp

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -3,25 +3,25 @@
 void main()
 {
 	clrscr();
-  int a[100],n,i,beg,end,mid,item;
+	int a[100],n,i,beg,end,mid,item;
 
-  cout<<"\n------------ BINARY SEARCH ------------ \n\n";
-  cout<<"Enter No. of Elements= ";
-  cin>>n;
+	cout<<"\n------------ BINARY SEARCH ------------ \n\n";
+	cout<<"Enter No. of Elements= ";
+	cin>>n;
 
-  cout<<"\nEnter Elements in Sorted Order=\n";
-  for(i=1;i<=n;i++)
-  {
-    cin>>a[i];
-  }
+  	cout<<"\nEnter Elements in Sorted Order=\n";
+  	for(i=1;i<=n;i++)
+  	{
+    		cin>>a[i];
+  	}
 
-  cout<<"\nEnter Item you want to Search= ";
-  cin>>item;
+  	cout<<"\nEnter Item you want to Search= ";
+  	cin>>item;
 
-  beg=1;
-  end=n;
+  	beg=1;
+  	end=n;
 
-  mid=(beg+end)/2;                       // Find Mid Location of Array
+  	mid=(beg+end)/2;                       // Find Mid Location of Array
 
 	while(beg<=end && a[mid]!=item)      // Compare Item and Value of Mid
 	{
@@ -31,15 +31,15 @@ void main()
 			end=mid-1;
 
 		mid=(beg+end)/2;
-}
+	}
 
-if(a[mid]==item)
-{
-	cout<<"\nData is Found at Location : "<<mid;
-}
-else
-{
-	cout<<"Data is Not Found";
-}
-	getch();
+	if(a[mid]==item)
+	{
+		cout<<"\nData is Found at Location : "<<mid;
+	}
+	else
+	{
+		cout<<"Data is Not Found";
+	}
+
 }
