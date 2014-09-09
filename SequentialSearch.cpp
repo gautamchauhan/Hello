c4b49aae59e864c3c3086fdfc42976082ee6c2c1
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 00:29:31 -0300

    Update SequentialSearch.cpp

--- SequentialSearch.cpp
+++ SequentialSearch.cpp
@@ -5,12 +5,12 @@ void main()
 	int a[100],n,i,item;
 	bool itemFound = true; //boolean variable
 
-	cout<<"\n------------ BINARY SEARCH ------------ \n\n";
+	cout<<"\n------------ SEQUENTIAL SEARCH ------------ \n\n";
 	cout<<"Enter No. of Elements= ";
 	cin>>n;
 
-	cout<<"\nEnter Elements in Sorted Order=\n";
-	for(i=1;i<=n;i++)
+	cout<<"\nEnter Elements\n";
+	for(i=0;i<n;i++)
 	{
 		cin>>a[i];
 	}
@@ -18,11 +18,11 @@ void main()
 	cout<<"\nEnter Item you want to Search= ";
 	cin>>item;
 
-	for (i=1; i<=n;i++)
+	for (i=0; i<n;i++)
 	{
 		if(a[i]== item)
 		{
-      			cout<<"\nData is Found at Location : "<<i;
+      			cout<<"\nData is Found at Location : "<<(i+1);
 		} 
 		else 
 		{
