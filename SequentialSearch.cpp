146c4632a11643ddcf5d4a6bff767df326e3f5a3
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 00:23:44 -0300

    Create SequentialSearch.cpp

---
+++ SequentialSearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream.h>
+#include<conio.h>
+
+void main()
+{
+clrscr();
+int a[100],n,i,item;
+bool itemFound = true; //boolean variable
+
+cout<<"\n------------ BINARY SEARCH ------------ \n\n";
+cout<<"Enter No. of Elements= ";
+cin>>n;
+
+cout<<"\nEnter Elements in Sorted Order=\n";
+for(i=1;i<=n;i++)
+{
+cin>>a[i];
+}
+
+cout<<"\nEnter Item you want to Search= ";
+cin>>item;
+
+for (i=1; i<=n;i++)
+{
+	if(a[i]== item)
+	{
+      	cout<<"\nData is Found at Location : "<<i;
+	} 
+	else 
+	{
+		itemFound = false;
+	}
+}
+if(itemFound == false)
+{
+cout<<"Data is Not Found";
+}
+}
+
