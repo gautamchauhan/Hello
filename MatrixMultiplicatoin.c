e4c7933a37173678e600fe7032574a32579f1f3a
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 01:01:40 -0300

    Update MatrixMultiplicatoin.c

--- MatrixMultiplicatoin.c
+++ MatrixMultiplicatoin.c
@@ -3,54 +3,55 @@
 int main()
 {
 	int size, i, j, k, sum = 0;
-  int first[10][10], second[10][10], multiply[10][10];
+	int first[10][10], second[10][10], multiply[10][10];
  
-  printf("Enter the number of rows and columns of first matrix\n");
-  scanf("%d", &size);
-  printf("Enter the elements of first matrix\n");
+	printf("Enter the number of rows and columns of first matrix\n");
+  	scanf("%d", &size);
+  	printf("Enter the elements of first matrix\n");
  
-  for (  i = 0 ; i < size ; i++ )
-  {
-    for ( j = 0 ; j < size ; j++ )
-    {
-      scanf("%d", &first[i][j]);
-    }
-  }
+  	for (  i = 0 ; i < size ; i++ )
+  	{
+    		for ( j = 0 ; j < size ; j++ )
+    		{
+      			scanf("%d", &first[i][j]);
+    		}
+  	}
  
-  printf("Enter the elements of second matrix\n");
+  	printf("Enter the elements of second matrix\n");
  
-  for ( i = 0 ; i < size ; i++ )
-  {
-   for ( j = 0 ; j < size ; j++ )
-   {
-        scanf("%d", &second[i][j]);
-   }
-  }
+  	for ( i = 0 ; i < size ; i++ )
+  	{
+   		for ( j = 0 ; j < size ; j++ )
+   		{
+        		scanf("%d", &second[i][j]);
+   		}
+  	}
  
-  for ( i = 0 ; i < size ; i++ )
-   {
-    for ( j = 0 ; j < size ; j++ )
-    {
-      for ( k = 0 ; k < size ; k++ )
-      {
-        sum = sum + first[i][k]*second[k][j];
-      }
-      multiply[i][j] = sum;
-      sum = 0;
-    }
-   }
+  	for ( i = 0 ; i < size ; i++ )
+   	{
+    		for ( j = 0 ; j < size ; j++ )
+    		{
+      			for ( k = 0 ; k < size ; k++ )
+      			{
+        			sum = sum + first[i][k]*second[k][j];
+      			}
+      			
+      			multiply[i][j] = sum;
+      			sum = 0;
+    		}
+   	}
  
-    printf("Product of entered matrices:-\n");
+    	printf("Product of entered matrices:-\n");
  
-    for ( i = 0 ; i < size ; i++ )
-    {
-      for ( j = 0 ; j < size ; j++ )
-      {
-        printf("%d\t", multiply[i][j]);
-      }
+    	for ( i = 0 ; i < size ; i++ )
+    	{
+      		for ( j = 0 ; j < size ; j++ )
+      		{
+        		printf("%d\t", multiply[i][j]);
+      		}
  
-      printf("\n");
-    }
+      		printf("\n");
+    	}
  
   return 0;
 }
