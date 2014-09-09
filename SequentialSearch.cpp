0e8e3f7d115d8ba4bd6e0d4afaf1ca44ee812bbc
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-09 00:26:08 -0300

    Update SequentialSearch.cpp

--- SequentialSearch.cpp
+++ SequentialSearch.cpp
@@ -3,37 +3,37 @@
 
 void main()
 {
-clrscr();
-int a[100],n,i,item;
-bool itemFound = true; //boolean variable
+	clrscr();
+	int a[100],n,i,item;
+	bool itemFound = true; //boolean variable
 
-cout<<"\n------------ BINARY SEARCH ------------ \n\n";
-cout<<"Enter No. of Elements= ";
-cin>>n;
+	cout<<"\n------------ BINARY SEARCH ------------ \n\n";
+	cout<<"Enter No. of Elements= ";
+	cin>>n;
 
-cout<<"\nEnter Elements in Sorted Order=\n";
-for(i=1;i<=n;i++)
-{
-cin>>a[i];
-}
-
-cout<<"\nEnter Item you want to Search= ";
-cin>>item;
-
-for (i=1; i<=n;i++)
-{
-	if(a[i]== item)
+	cout<<"\nEnter Elements in Sorted Order=\n";
+	for(i=1;i<=n;i++)
 	{
-      	cout<<"\nData is Found at Location : "<<i;
-	} 
-	else 
-	{
-		itemFound = false;
+		cin>>a[i];
 	}
-}
-if(itemFound == false)
-{
-cout<<"Data is Not Found";
-}
+
+	cout<<"\nEnter Item you want to Search= ";
+	cin>>item;
+
+	for (i=1; i<=n;i++)
+	{
+		if(a[i]== item)
+		{
+      			cout<<"\nData is Found at Location : "<<i;
+		} 
+		else 
+		{
+			itemFound = false;
+		}
+	}
+	if(itemFound == false)
+	{
+		cout<<"Data is Not Found";
+	}
 }
 
