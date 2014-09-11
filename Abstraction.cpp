684666c24377049b7f207a52deb06e439eafa9c9
Gautam Chauhan <gautam.chauhan@aol.com>
2014-09-11 15:38:42 -0300

    Create Abstraction.cpp

---
+++ Abstraction.cpp
@@ -0,0 +1,29 @@
+class Area
+{
+	private:
+		int x, y, area;
+	public:
+		void setValue(int width, int height);
+		int getArea();
+};
+
+void Area::setValue(int width, int height)
+{
+	x = width;
+	y = height;
+}
+
+void Area::getArea():
+{
+	return (x*y);
+}
+
+int main()
+{
+
+	Area a;
+	a.setValue(5,7);
+	a.getArea();
+	
+	return 0;
+}
